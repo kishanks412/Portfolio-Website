e0b21bc8287c1f5b6c7f0b1686b980c4a5c8c2ac
kishan <kkseth412@gmail.com>
2023-08-31 14:03:03 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/Reducer";
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
